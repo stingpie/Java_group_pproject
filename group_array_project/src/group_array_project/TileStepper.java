e556327a4e549a105bc9ff29adaf7c6f3b72face
stingpie <53981910+stingpie@users.noreply.github.com>
2021-02-24 10:47:49 -0600

    Update TileStepper.java

--- group_array_project/src/group_array_project/TileStepper.java
+++ group_array_project/src/group_array_project/TileStepper.java
@@ -1,10 +1,11 @@
 package group_array_project;
 
 public interface TileStepper {
-	public int[] currentPos= {0,0};
-	public int[][] confirmedSteps= new int[0][2]; // each confirmed step should be a pair of cooridnates on the grid
-	public int[][] testSteps= new int[0][2]; //The steps that should be tested in the next iteration
-	public int[][] grid = new int[5][5];
+	// IMPLEMENT THESE
+	//public int[] currentPos= {0,0};
+	//public int[][] confirmedSteps= new int[0][2]; // each confirmed step should be a pair of cooridnates on the grid
+	//public int[][] testSteps= new int[0][2]; //The steps that should be tested in the next iteration
+	//public int[][] grid = new int[5][5];
 	
 	public abstract void findPossibleSteps(); // look along the edges of the current path, and add them to test steps if they're new. 
 	
