96d20c99ca26ba0929d2eae3e008c8e74993c393
Shraiwi <Shraiwi@users.noreply.github.com>
2021-03-01 14:11:26 -0600

    Renamed some files, added UML pics

--- group_array_project/src/group_array_project/StringToArray.java
+++ group_array_project/src/group_array_project/StringToArray.java
@@ -1,7 +1,19 @@
 package group_array_project;
 
-public interface StringToArray {
+public class StringToArray implements StringToArrayInterface {
+	public int[][] convert(String input, String rowSeparator, String columnSeparator) {
+		String[] rows = input.split(rowSeparator);
+		int[][] ret = new int[rows.length][];
 
-	public int[][] convert(String input, String rowSeparator, String columnSeparator);
 
+		for (int r = 0; r < rows.length; r++) {
+			String[] row = rows[r].split(columnSeparator);
+			ret[r] = new int[row.length];
+			for (int c = 0; c < row.length; c++) {
+				ret[r][c] = Integer.parseInt(row[c]);
+			}
+		}
+
+		return ret;
+	}
 }
---
+++ group_array_project/src/group_array_project/StringToArrayInterface.java
@@ -0,0 +1,7 @@
+package group_array_project;
+
+public interface StringToArrayInterface {
+
+	public int[][] convert(String input, String rowSeparator, String columnSeparator);
+
+}
